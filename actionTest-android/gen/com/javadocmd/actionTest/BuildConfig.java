17325ce94130fabc5e5b1a0bb7c425f9eb097675
Tyler <javadocmd@gmail.com>
2014-01-31 21:18:29 -0700

    Commit for article publish.

---
+++ actionTest-android/gen/com/javadocmd/actionTest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.javadocmd.actionTest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ actionTest-desktop/src/com/javadocmd/actionTest/Main.java
@@ -0,0 +1,16 @@
+package com.javadocmd.actionTest;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "actionTest";
+		cfg.useGL20 = false;
+		cfg.width = 400;
+		cfg.height = 200;
+		
+		new LwjglApplication(new ActionTestGame(), cfg);
+	}
+}
